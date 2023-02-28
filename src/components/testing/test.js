c8807e92a65bc509157a89b8f94a5f6162581c68
Hector Kurtyanek <hkurtyanek@adaptavist.com>
2023-02-28 21:42:51 +0000

    test out api

---
+++ src/components/testing/test.js
@@ -0,0 +1,24 @@
+const test_url = "https://geo.ipify.org/api/v2/country,city?";
+const apiKey = "at_gtxE4ztYdEzHBWpvl9jZHin1qdaBW";
+const ipAddress = "8.8.8.8";
+
+const concat_url = test_url + "apiKey=" + apiKey + "&ipAddress=" + ipAddress;
+console.log(concat_url);
+
+// const headersList = {
+//   Accept: "*/*",
+// };
+
+fetch(concat_url, {
+  method: "GET",
+})
+  .then((response) => {
+    if (!response.ok) {
+      throw Error("something went wrong");
+    }
+    return response.json();
+  })
+  .then((data) => {
+    console.log("data below");
+    console.log(data);
+  });
