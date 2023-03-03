3cfaca7747078b95dd65dc054b3bd03167fbfb4b
Hector Kurtyanek <hkurtyanek@adaptavist.com>
2023-03-03 21:37:25 +0000

    update search bar component

--- src/components/layout/SearchBar.js
+++ src/components/layout/SearchBar.js
@@ -1,12 +1,37 @@
 import "../layout/SearchBar.css";
+import { useState } from "react";
 
 function SearchBar() {
+  const api = "https://geo.ipify.org/api/v2/country,city?";
+  const apiKey = "at_gtxE4ztYdEzHBWpvl9jZHin1qdaBW";
+  const [ipAddress, setipAddress] = useState("8.8.8.8");
+
+  function handleIP(event) {
+    const input = event.target.value;
+    setipAddress(input);
+  }
+  function handleSubmit(event) {
+    event.preventDefault();
+    fetch(api + "apiKey=" + apiKey + "&ipAddress=" + ipAddress)
+      .then((response) => {
+        if (!response.ok) {
+          throw Error("something went wrong");
+        }
+        return response.json();
+      })
+      .then((data) => {
+        console.log("data below");
+        console.log(data);
+      });
+  }
+
   return (
     <div className="search">
-      <form>
-        <input // set input style based on state
+      <form onSubmit={handleSubmit}>
+        <input
+          onChange={handleIP} // set input style based on state
           type="text"
-          placeholder="Shorten a link here..."
+          placeholder="Search for an IP..."
         />
         <button className="search-btn" type="submit">
           Shorten it!
