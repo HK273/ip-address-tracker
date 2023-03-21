0dcadd60607e06106032db7162580fc342a3a727
Hector Kurtyanek <hkurtyanek@adaptavist.com>
2023-03-21 08:49:12 +0000

    add some experiments

--- src/components/lift-state/childTwo.js
+++ src/components/lift-state/childTwo.js
@@ -1,5 +1,10 @@
-function ChildTwo({ data }) {
-  return <div>{data.weight}</div>;
+function ChildTwo({ data, dataTwo }) {
+  return (
+    <div>
+      <h1>{data.weight}</h1>
+      <h1>{dataTwo.name}</h1>
+    </div>
+  );
 }
 
 export default ChildTwo;
--- src/components/lift-state/parent.js
+++ src/components/lift-state/parent.js
@@ -4,29 +4,45 @@ import ChildTwo from "./childTwo";
 
 function Parent() {
   const [data, setData] = useState(null);
+  const [dataTwo, setDataTwo] = useState(null);
 
   async function searchAPI() {
     const api = "https://pokeapi.co/api/v2/pokemon/1";
-    try {
-      const response = await fetch(api);
-      if (!response.ok) {
-        throw new Error(response.statusText);
-      }
-      const data = await response.json();
-      console.log(data);
-      setData(data);
-    } catch (error) {
-      console.error(error);
-    }
+    const apiTwo = "https://pokeapi.co/api/v2/pokemon/2";
+    fetch(api)
+      .then((response) => {
+        if (!response.ok) {
+          throw Error("something went wrong");
+        }
+        return response.json();
+      })
+      .then((data) => {
+        console.log("data below");
+        console.log(data);
+        setData(data);
+      });
+    fetch(apiTwo)
+      .then((response) => {
+        if (!response.ok) {
+          throw Error("something went wrong");
+        }
+        return response.json();
+      })
+      .then((dataTwo) => {
+        console.log("data below");
+        console.log(dataTwo);
+        setDataTwo(dataTwo);
+      });
   }
 
+  // Doesn't work, find out why
   return (
     <div>
       <button onClick={searchAPI}>Click This</button>
       {data && (
         <>
           <ChildOne data={data} />
-          <ChildTwo data={data} />
+          <ChildTwo data={data} dataTwo={dataTwo} />
         </>
       )}
     </div>
