ce92cb4145a98475c65083c27749f4d8c518a183
Hector Kurtyanek <hkurtyanek@adaptavist.com>
2023-03-25 13:01:03 +0000

    rename-folder

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import MapComponent from "./components/map/Map";
 import HeaderSection from "./components/layout/Header";
-import Parent from "./components/lift-state/parent";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
