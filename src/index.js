5f023eacbda20551ab9e9a3323c7c1b072e838e9
Hector Kurtyanek <hkurtyanek@adaptavist.com>
2023-03-25 18:57:29 +0000

    update form fields

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import MapComponent from "./components/map/Map";
 import HeaderSection from "./components/layout/Header";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
